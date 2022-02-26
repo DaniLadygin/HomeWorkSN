722bd6f3a381b9df57c40dd5131baa3f455a1c94
Daniil Ladygin <95340198+DaniLadygin@users.noreply.github.com>
2022-02-26 09:35:40 +0300

    Add files via upload
    node_modules не дает подгрузить

---
+++ DB.js
@@ -0,0 +1,33 @@
+const db = {
+  images: [
+    {
+      src: 'https://cdn.pixabay.com/photo/2020/05/08/16/06/dog-5146351_960_720.jpg',
+      description: 'dog',
+    },
+    {
+      src: 'https://cdn.pixabay.com/photo/2021/02/23/09/26/cat-6042858_960_720.jpg',
+      description: 'cat',
+    },
+
+    {
+      src: 'https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg',
+      description: 'sunset',
+    },
+    {
+      src: 'https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_960_720.jpg',
+      description: 'ocean',
+    },
+    {
+      src: 'https://cdn.pixabay.com/photo/2014/10/07/13/48/mountain-477832_960_720.jpg',
+      description: 'mountain',
+    },
+    {
+      src: 'https://cdn.pixabay.com/photo/2019/03/09/21/30/downtown-4045035_960_720.jpg',
+      description: 'dubai',
+    }
+  ],
+}
+
+module.exports = {
+  db,
+}
